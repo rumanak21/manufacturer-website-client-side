066534e1ab4516641b1f497d0a18b291ee87ae2c
rumanak21 <rumana2k20@gmail.com>
2022-05-25 03:04:48 +0600

    Hero Section added

---
+++ src/pages/Home/HeroSection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HeroSection = () => {
+    return (
+        <div class="hero min-h-screen bg-base-200 margin=0">
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <img src="https://www.powertoolsuk.co.uk/media/catalog/product/cache/94a3e057056f6e2e92c241d8127151dc/b/o/bosch_gas35hafc_dust_extractor_power_tools_uk_0321_hero_1.jpg" />
+                <div>
+                    <h1 class="text-4xl font-bold">A FLEXIBLE AND SERVICE FOCUSED APPROACH
+                        TO SUPPLY TOOLS AND ELECTRICAL EQUIPMENT	</h1>
+                    <p class="py-6">For 15 years, the Electrical  Manufacturer Tools team has supplied the manufacturing industry with ‘ the right tool for the job’. As customer service-based industrial suppliers, we can offer a wealth of knowledge and expertise in many sectors. Please visit us at our tool shop or email us.</p>
+                    <button class="btn btn-success"> Shp Now</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default HeroSection;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from './Banner';
 import BusinessSummary from './BusinessSummary';
+import HeroSection from './HeroSection';
 import Reviews from './Reviews';
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <BusinessSummary></BusinessSummary>
+            <HeroSection></HeroSection>
             <Reviews></Reviews>
         </div>
     );
