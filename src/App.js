55eb1dd6114fbad8ba07c2acdffcabab91a78a70
rumanak21 <rumana2k20@gmail.com>
2022-05-24 06:00:48 +0600

    Route setup completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home';
 import Footer from './pages/Shared/Footer/Footer';
+import Blog from './pages/Shared/Header/Blog';
 import Header from './pages/Shared/Header/Header';
+import SignIn from './pages/Shared/SignIn/SignIn';
+import SignUp from './pages/Shared/SignIn/SignUp';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Home></Home>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="blog" element={<Blog />} />
+        <Route path="signin" element={<SignIn />} />
+        <Route path="signup" element={<SignUp />} />
+      </Routes>
+
       <Footer></Footer>
     </div>
   );
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
@@ -9,23 +10,24 @@ const Header = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Home</a></li>
-                        <li><a>Blog</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="blog">Blog</Link></li>
 
                     </ul>
                 </div>
-                <a class="btn btn-ghost normal-case text-xl">Electric Tools Manufacturer
-                </a>
+                <Link to="/" class="btn btn-ghost normal-case text-xl">Electric Tools Manufacturer
+                </Link>
             </div>
             <div class="navbar-center hidden lg:flex">
                 <ul class="menu menu-horizontal p-0">
-                    <li><a>Home</a></li>
-                    <li><a>Blog</a></li>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="blog">Blog</Link></li>
                 </ul>
             </div>
             <div class="navbar-end ">
-                <button class="btn btn-active btn-ghost ">Sign In</button>
-                <button class="btn btn-active btn-ghost ">Sign UP</button>
+                <Link to="signin"><button class="btn btn-active btn-ghost ">Sign In</button></Link>
+                <Link to="signup"><button class="btn btn-active btn-ghost ">Sign UP</button></Link>
+                
 
             </div>
         </div>
